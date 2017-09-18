d99c77389ad65baf93e957faba5617f8f988a35a
Fan Zhang <zhafang@google.com>
2017-09-18 14:08:59 -0700

    Added comment

--- examples/js/loaders/draco/DRACOLoader.js
+++ examples/js/loaders/draco/DRACOLoader.js
@@ -29,6 +29,8 @@ THREE.DRACOLoader = function(dracoPath, dracoDecoderType, manager) {
         (dracoDecoderType !== undefined) ? dracoDecoderType : {};
     this.drawMode = THREE.TrianglesDrawMode;
     this.dracoSrcPath = (dracoPath !== undefined) ? dracoPath : './';
+    // If draco_decoder.js or wasm code is already loaded/included, then do
+    // not dynamically load the decoder.
     if (typeof DracoDecoderModule === 'undefined') {
       THREE.DRACOLoader.loadDracoDecoder(this);
     }
